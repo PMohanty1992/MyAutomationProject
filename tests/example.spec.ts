d4ca16fe0eddb47bea8103d86fdf254d2a1a1599
PMohanty1992 <pragyanmohanty1992@gmail.com>
2026-09-18 18:51:56 -0600

    Update test case to verify Wikipedia page title
    Replaced existing tests with a new test for Wikipedia page title.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('Simple Proof of Concept Pass Test', async ({ page }) => {
+  // Go to Wikipedia
+  await page.goto('https://wikipedia.org');
+  
+  // Verify the page title has the word Wikipedia in it
+  await expect(page).toHaveTitle(/Wikipedia/);
 });
